5b8f6fbd1cd6b6ea53d46b61dba19b172a563ece
Daniel Oy <daniel-oy-mis@hotmail.com>
2022-04-05 00:00:38 -0500

    feat: statistics by category added
    implementation of the methods for receive the statistics of the most liked or disliked posts by categories.

--- API/Controllers/StatisticsController.cs
+++ API/Controllers/StatisticsController.cs
@@ -1,14 +1,13 @@
 ﻿using API.Dtos;
-using API.Helpers;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,14 +20,17 @@ namespace API.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public StatisticsController(UserManager<User> userManager, IUnitOfWork unitOfWork)
+        public StatisticsController(UserManager<User> userManager, IUnitOfWork unitOfWork, IMapper mapper)
         {
             _userManager = userManager;
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         [HttpGet("GetUserStatistics")]
+        [Authorize(Roles = "Administrator")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<StatisticDto>))]
         public async Task<ActionResult<IEnumerable<StatisticDto>>> GetUserStatistics()
         {
@@ -45,6 +47,7 @@ namespace API.Controllers
         }
 
         [HttpGet("GetLikeStatistics")]
+        [Authorize(Roles = "Administrator")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<StatisticDto>))]
         public async Task<ActionResult<IEnumerable<StatisticDto>>> GetLikeStatistics()
         {
@@ -54,6 +57,7 @@ namespace API.Controllers
         }
 
         [HttpGet("GetDislikeStatistics")]
+        [Authorize(Roles = "Administrator")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<StatisticDto>))]
         public async Task<ActionResult<IEnumerable<StatisticDto>>> GetDislikeStatistics()
         {
@@ -73,6 +77,75 @@ namespace API.Controllers
                 })
                 .ToList();
             return data;
+        }
+
+        [HttpGet("GetLikesByCategoryStatistics")]
+        [Authorize(Roles = "Administrator")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ReactionPostDto>))]
+        public async Task<ActionResult<IEnumerable<ReactionPostDto>>> GetLikesByCategoryStatistics()
+        {
+            var categories = await _unitOfWork.Tags.GetAllByExpression();
+            var reactionPosts = new List<ReactionPostDto>();
+
+            foreach (var category in categories)
+            {
+                var post = new ReactionPostDto
+                {
+                    Category = category.Name
+                };
+
+                var postDto = await GetPostByCategoryAndReactionSort(category, "MostPopular");
+                post.ImageUrl = postDto?.PictureUrl;
+                post.ReactionCount = postDto?.LikeCount ?? 0;
+                post.PostId = postDto?.Id ?? 0;
+                post.PostDescription = postDto.Description;
+
+                reactionPosts.Add(post);
+            }
+
+            return reactionPosts;
+        }
+
+        [HttpGet("GetDislikesByCategoryStatistics")]
+        [Authorize(Roles = "Administrator")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ReactionPostDto>))]
+        public async Task<ActionResult<IEnumerable<ReactionPostDto>>> GetDislikesByCategoryStatistics()
+        {
+            var categories = await _unitOfWork.Tags.GetAllByExpression();
+            var reactionPosts = new List<ReactionPostDto>();
+
+            foreach (var category in categories)
+            {
+                var post = new ReactionPostDto
+                {
+                    Category = category.Name
+                };
+
+                var postDto = await GetPostByCategoryAndReactionSort(category, "LeastPopular");
+                post.ImageUrl = postDto?.PictureUrl;
+                post.ReactionCount = postDto?.DislikeCount ?? 0;
+                post.PostId = postDto?.Id ?? 0;
+                post.PostDescription = postDto.Description;
+
+                reactionPosts.Add(post);
+            }
+
+            return reactionPosts;
+        }
+
+        private async Task<PostDto> GetPostByCategoryAndReactionSort(Tag category, string sort)
+        {
+            var postParams = new PostSpecParams
+            {
+                PageIndex = 1,
+                PageSize = 1,
+                Tag = category.Name,
+                Sort = sort
+            };
+            var spec = new PostPaginationSpecification(postParams);
+            var posts = await _unitOfWork.Posts.GetAllBySpecification(spec);
+            var data = _mapper.Map<IEnumerable<Post>, IEnumerable<PostDto>>(posts);
+            return data.FirstOrDefault();
         }
     }
 }
---
+++ API/Dtos/ReactionPostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Dtos
+{
+    public class ReactionPostDto
+    {
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+        public int ReactionCount { get; set; }
+        public int PostId { get; set; }
+        public string PostDescription { get; set; }
+    }
+}
